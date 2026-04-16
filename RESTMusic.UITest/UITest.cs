61b6c1b9a7fdce1d6feab8e7c60d7b1ef1377a09
Eylem003 <eylem310305@gmail.com>
2026-04-16 20:45:03 +0200

    ja, det virker!

--- RESTMusic.UITest/UITest.cs
+++ RESTMusic.UITest/UITest.cs
@@ -1,10 +1,30 @@
-﻿namespace RESTMusic.UITest;
+﻿using Xunit;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
 
-public class UITest
+namespace RESTMusic.UITest
 {
-    [Fact]
-    public void Test1()
+    public class UITest
     {
+        [Fact]
+        public void Test_GetAllMusic_Button_Works()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://127.0.0.1:5500/Exercise.html");
+            Thread.Sleep(2000);
 
+            var button = driver.FindElement(By.XPath("//button[contains(text(),'Get all Music')]"));
+
+            button.Click();
+
+            Thread.Sleep(2000);
+
+            var listItems = driver.FindElements(By.TagName("li"));
+
+            Assert.True(listItems.Count >= 0);
+
+            driver.Quit();
+        }
     }
 }
